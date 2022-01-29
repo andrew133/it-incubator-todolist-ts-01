af3d81361e36d79f0bda61ae461a992d74f83105
Andrey <55235312+andrew133@users.noreply.github.com>
2022-01-29 18:28:47 +0300

    11

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
                 <ul>
                     <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
                     <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+              
                     <li><input type="checkbox" checked={false}/> <span>React</span></li>
                 </ul>
                 <div>
